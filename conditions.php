225ce121d670d5ce7cbfb907968b286eff585d28
Denis Glukhov <archirius@gmail.com>
2021-12-04 12:07:31 +0300

    Добавлены новые файлы

---
+++ conditions.php
@@ -0,0 +1,12 @@
+<?php 
+$age = 19;
+
+if ($age < 7) {
+echo "Вы дошкольник";
+echo "<br>";
+} else if ($age > 6 && $age < 19) {
+    echo "Вы школьник";
+    echo "<br>";
+} else {
+    echo "Школа позади";
+}
