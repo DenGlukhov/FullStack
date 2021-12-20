1e41b21e8c025a7e480f01d4c8f1e0455db61946
Denis Glukhov <archirius@gmail.com>
2021-12-20 23:10:57 +0300

    Изучаем запросы к базе данных их PHP

--- functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function getSum ($a, $b) {
-    return $a + $b;
-}
-
-echo getSum(55, 60);
--- loop.php
+++
@@ -1,7 +0,0 @@
-<?php
-$temperature = -50;
-while ($temperature <= 50) {
-    $celsius = round(($temperature - 32) * 5 / 9, 2);
-    echo "Температура: $temperature °F : $celsius °С" . '<br>';
-    $temperature++;
-}
